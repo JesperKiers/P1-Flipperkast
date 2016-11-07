884ecc01656dda7f95865c7c222d2bbeb010f7e3
JesperKiers <jeppywho@gmail.com>
2016-11-07 13:12:12 +0100

    comments added

--- Assets/flipperkast dingen/scripts/DeathZone.cs
+++ Assets/flipperkast dingen/scripts/DeathZone.cs
@@ -19,6 +19,6 @@ public class DeathZone : MonoBehaviour {
         Destroy(c.gameObject);//deletes object that collides with the one with this component
         GameObject.Find("Manager").GetComponent<GameManager>().lives -= 1;//takes one life 
 
-        GameObject.Find("launcher").GetComponent<PullSpring>().SpawnNewBall();
+        GameObject.Find("launcher").GetComponent<PullSpring>().SpawnNewBall();//calls the void for a new ball
     }
 }
